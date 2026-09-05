4da470bc36cd4a6ec62850ba2da359fc127f96cf
Karim <karimahmedrageb27.6.2006@gmail.com>
2026-09-05 04:01:12 +0300

    message

--- bin/2nd.dart
+++ bin/2nd.dart
@@ -1,10 +1,104 @@
 // ignore: file_names
 void  main()
+
 {
-  int x = 20 ;
-  print(x);
+    // int x = 20  ;
+    // int y = 30 ;
+//   print(x);
 
-print (x is int) ;
-print (x is String) ;
+// print (x is int) ;
+// print (x is! String) ;
+//////////////////////////
+// if (y>x)
+// {
+//   print("y is greater than x");
+// }
+// else
+// {
+//   print("x is greater than y");
+// }
+// if (y>x)
+// {
+//   print("y is greater than x");
+// }
+// else if(x>y) 
+// {
+//   print("x is greater than y");
+// }
+// else
+// {
+//   print("x is equal to y");
+// }
+// y > x ? print("y is greater than x") : print("x is greater than y") ;
+// String day = "Sunday" ;
+// switch(day)
+// {
+//   case "Monday":
+//   print("Today is Monday");
+//   break;
+//   case "Tuesday":
+//   print("Today is Tuesday");
+//   break;
+//   case "Wednesday":
+//   print("Today is Wednesday");
+//   break;
+// }
+// String message = switch(day)
+// {
+//   "Monday" => "Today is Monday switch ",
+//   "Tuesday" => "Today is Tuesday",
+//   "Wednesday" => "Today is Wednesday",
+//   _ => "Invalid day"
+// };
+// print(message);
+// printname();
+// String Mohammed = getname();
+// print(Mohammed);
+// printname2("Karim");
+// printFullName(
+//   firstName: "Karim",
+//   lastName: "Ahmed"
+// ) ;
+print(calculate(
+  x : 50 ,
+  y : 80 ,
+  operation : '*' , 
+)) ;
+
 }
-
+// void printname()
+// {
+//   print("My name is Karim");
+// }
+// String getname()
+// {
+//   return "My name is Karim (get)";
+// }
+// void printname2(String name)
+// {
+//   print("My name is string $name");
+// }
+// void printFullName({
+//   required String firstName,
+//   required String lastName,
+// }) {
+//   print("My name is $firstName $lastName");
+// }
+int calculate ( {
+  required int x , required int y,required String operation}
+)
+{
+  int result = 0 ;
+  switch (operation)
+  {
+    case '+' :
+      result = x + y ;
+      case '-' :
+      result = x - y ;  
+      case '*' :
+      result = x * y ;
+      case '/' :
+      result = x ~/ y ;
+  }
+  return result ;
+}

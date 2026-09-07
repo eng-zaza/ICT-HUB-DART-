0c48eeccf6f74c85666079abc4718491c8ff9020
Karim <karimahmedrageb27.6.2006@gmail.com>
2026-09-07 22:47:31 +0300

    task3

---
+++ bin/3rd/task_3 s3 Abstraction.dart
@@ -0,0 +1,50 @@
+abstract class HospitalTeam {
+  String name;
+
+  HospitalTeam(this.name);
+
+  void work();
+}
+
+class Doctor extends HospitalTeam {
+  Doctor(String name) : super(name);
+
+  @override
+  void work() {
+    print("$name is treating patients");
+  }
+}
+
+class Nurse extends HospitalTeam {
+  Nurse(String name) : super(name);
+
+  @override
+  void work() {
+    print("$name is taking care of patients");
+  }
+}
+
+class Receptionist extends HospitalTeam {
+  Receptionist(String name) : super(name);
+
+  @override
+  void work() {
+    print("$name is registering patients");
+  }
+}
+
+void main() {
+  Doctor doctor = Doctor("Ahmed");
+  Nurse nurse = Nurse("judy");
+  Receptionist receptionist = Receptionist("Karim");
+Doctor doctor2 = Doctor("nada");
+  Nurse nurse2 = Nurse("nadine");
+  Receptionist receptionist2 = Receptionist("Karim");
+  doctor.work();
+  nurse.work();
+  receptionist.work();
+  print("(-----------/-/-/-/-/)");
+  doctor2.work();
+  nurse2.work();
+  receptionist2.work();
+}
